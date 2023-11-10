5a9a70885e73f30c4295d3ec49e8f4bd9b2ef567
newblow <bollogmx@gmail.com>
2023-11-10 09:13:56 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Management project</title>
+    <link rel="stylesheet" href="./assets/css/styles.css">
+    <script defer src="./assets/js/main.js"></script>
+</head>
+<body>
+    
+    <h1>Let's time to play with SQL !</h1>
+
+
+</body>
+</html>
